61d98d47bb70d32a4b75ee56944e2ae67146102c
Edwin574 <owinoedwin574@gmail.com>
2023-11-07 09:17:33 +0300

    starting linked lists

---
+++ LinkedList/singlelinkedList.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+    def getData(self):
+        pass
+
+    def setData(self):
+        pass
+
+class SinglyList:
+    pass
